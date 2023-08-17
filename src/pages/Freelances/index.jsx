fa80ca13d68547cfc5f920b90d3e2681dda9b96e
David De Castro <segphault@gmail.com>
2023-08-17 15:18:22 +0200

    Découverte propTypes, defaultProps

--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,7 +1,37 @@
+import DefaultPicture from '../../assets/breakcore.jpg';
+
+import Card from '../../components/Card';
+
+const freelanceProfiles = [
+    {
+        name: 'Jane Doe',
+        jobTitle: 'Devops',
+        picture: DefaultPicture
+    },
+    {
+        name: 'John Doe',
+        jobTitle: 'Développeur frontend',
+        picture: DefaultPicture
+    },
+    {
+        name: 'Jeanne Biche',
+        jobTitle: 'Développeuse Fullstack',
+        picture: DefaultPicture
+    }
+];
+
 function Freelances() {
     return (
         <div>
             <h1>Freelances</h1>
+            {freelanceProfiles.map((profile, index) => (
+                <Card
+                    key={`${profile.name}-${index}`}
+                    label={profile.jobTitle}
+                    picture={profile.picture}
+                    title={profile.name}
+                />
+            ))}
         </div>
     );
 }
